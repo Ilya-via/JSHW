df1458cfb89e8826e24d9c5d02a366123cf1af17
Ilya-via <ilya95v@gmail.com>
2019-10-19 02:35:12 +0300

    all without - 11, change massive name

--- JS3/src/js/script10.js
+++ JS3/src/js/script10.js
@@ -1,12 +1,13 @@
 console.log('%cЗадание 10', 'padding: 20px 0 0 0; font-weight: bold;');
-var arrr = [1, -8, 0, 13, 76, 8, 7, 0, 22, 0, 2, -2, 2];
-var b = arrr.length;
-var firstValue = arrr.indexOf(0, 0);
-var lastValue = arrr.lastIndexOf(0, b - 1);
+var massiveForSumm = [1, -8, 0, 13, 76, 8, 7, 0, 22, 0, 2, -2, 2];
+console.log(massiveForSumm);
+var b = massiveForSumm.length;
+var firstValue = massiveForSumm.indexOf(0, 0);
+var lastValue = massiveForSumm.lastIndexOf(0, b - 1);
 
 var s = 0;
 for (var i = firstValue; i <= lastValue; i++) {
-    for (var j = arrr[i]; i <= lastValue; i++) {
+    for (var j = massiveForSumm[i]; i <= lastValue; i++) {
         s += j;
         break;
     }
